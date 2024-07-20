9c01e3b450b84781fa2de22ae58a7363950351a8
zack256 <20821071+zack256@users.noreply.github.com>
2024-07-20 00:44:15 -0400

    lzw simple decode

--- lzw.py
+++ lzw.py
@@ -2,8 +2,8 @@ import string
 
 def simple_lzw_encode(s, starting_dictionary):
 
-    dictionary = starting_dictionary.copy()
     encoding = []
+    dictionary = starting_dictionary.copy()
     next_code = len(dictionary)
 
     z = 0
@@ -25,21 +25,51 @@ def simple_lzw_encode(s, starting_dictionary):
     return encoding
 
 
-def simple_lzw_decode():
-    pass
+def simple_lzw_decode(encoding, starting_dictionary):
+    
+    decoding = ""
+    dictionary = starting_dictionary.copy()
+    next_code = len(dictionary)
+
+    z = 0
+    conjecture = None
+    while z < len(encoding):
+        code = encoding[z]
+        if code in dictionary:
+            if z:
+                dictionary[next_code] = conjecture + dictionary[code][0]
+                next_code += 1
+        else:
+            dictionary[next_code] = conjecture + conjecture[0]
+            next_code += 1
+        conjecture = dictionary[code]
+        decoding += dictionary[code]
+        z += 1
+
+    return decoding
+
+def test(s, starting_dictionary):
+    encoded = simple_lzw_encode(s, starting_dictionary)
+    print("Encoded:", encoded)
+    decoded = simple_lzw_decode(encoded, { v : k for k, v in starting_dictionary.items() })
+    print("Decoded:", decoded)
+    if s == decoded:
+        print("Match!")
+    else:
+        print("No match!")
 
 # https://marknelson.us/posts/2011/11/08/lzw-revisited
-print(simple_lzw_encode(
+test(
     "ABBABBBABBA",
     {"A" : 0, "B" : 1}
-))
+)
 # https://en.wikipedia.org/wiki/Lempel-Ziv-Welch#Example
-print(simple_lzw_encode(
+test(
     "TOBEORNOTTOBEORTOBEORNOT",
     {ch : i for i, ch in enumerate(string.ascii_uppercase)}
-))
+)
 # https://stackoverflow.com/q/63493612
-print(simple_lzw_encode(
+test(
     "ababcbababaaaaaaa",
     {"a" : 0, "b" : 1, "c" : 2}
-))
+)
